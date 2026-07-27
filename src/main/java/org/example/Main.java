325c7d942e8c9d3147fa42772f62fb2306ccba40
kadersutlu <sutlukader@gmail.com>
2026-07-27 21:38:21 +0300

    feat: implement screenshot capture with auto-directory creation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,34 @@
 package org.example;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com");
+
+        File directory = new File("screenshots");
+        if(!directory.exists()){
+            directory.mkdir();
+        }
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+
+        File srcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File targetFile=new File("screenshots/saucedemo_screenshot.png");
+
+        Files.copy(srcFile.toPath(),targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        System.out.println("Screenshot taken successfully"+targetFile.getAbsolutePath());
+
+        driver.quit();
     }
 }
